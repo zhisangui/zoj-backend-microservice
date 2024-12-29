5f1e85f5539c7af8dc8330cef96659e16723db2f
zhisangui <2061066533@qq.com>
2024-12-29 17:15:36 +0800

    修改配置

--- zoj-backend-judge-service/src/main/java/com/zhisangui/zojbackendjudgeservice/judge/codesandbox/impl/RemoteCodeSandBox.java
+++ zoj-backend-judge-service/src/main/java/com/zhisangui/zojbackendjudgeservice/judge/codesandbox/impl/RemoteCodeSandBox.java
@@ -15,12 +15,18 @@ import zojbackendmodel.model.codesandbox.ExecuteCodeResponse;
  * @author zsg
  */
 public class RemoteCodeSandBox implements CodeSandBox {
+    @Value("${remote.codesandbox.host:localhost}")
+    private String host;
+
+    @Value("${remote.codesandbox.port:8081}")
+    private int port;
+
     @Override
     public ExecuteCodeResponse doJudge(ExecuteCodeRequest executeCodeRequest) {
         System.out.println("remote code sandbox");
         String jsonStr = JSONUtil.toJsonStr(executeCodeRequest);
-        // todo：修改为你的代码沙箱服务，以下是一个示例
-        String body = HttpUtil.createPost("http://localhost:8081/executeCode").body(jsonStr).execute().body();
+        // ★ 注意上线的时候ip必须修改
+        String body = HttpUtil.createPost("http://" + host + ":" + port + "/executeCode").body(jsonStr).execute().body();
         System.out.println("jsonStr:" + jsonStr);
         System.out.println(body);
         return JSONUtil.toBean(body, ExecuteCodeResponse.class);
